eddc5ab88283a5a164726b6ec41d0238332ec096
rafi <pawanrajput852710@gmail.com>
2023-10-12 18:46:07 +0530

    done

--- src/component/FormPage.jsx
+++ src/component/FormPage.jsx
@@ -340,7 +340,7 @@ console.warn(publicationData,postTypeData)
           />
         </div>
 
-        <div  style={{ width: '800px' }}>
+        <div  style={{ width: '1500px' }}>
         <ReactQuill
             value={richText}
             onChange={handleTextChange}
