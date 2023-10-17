e7471d0d68b1e6880fa8aaad0eea34f599895adf
rafi <pawanrajput852710@gmail.com>
2023-10-17 10:37:21 +0530

    done

--- src/component/Updatearticle.jsx
+++ src/component/Updatearticle.jsx
@@ -198,7 +198,7 @@ function Updatearticle() {
         .from('articles')
         .update(updatedArticle)
         .eq('article_id', articleId)
-        .auth(data1)// Make an authenticated request with the token in the headers
+        // Make an authenticated request with the token in the headers
        
        
         
