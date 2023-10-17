6f81a9386e36835dd8b7f2d1c800e322d8935ef9
rafi <pawanrajput852710@gmail.com>
2023-10-17 14:33:09 +0530

    done

--- src/component/HomePage.jsx
+++ src/component/HomePage.jsx
@@ -171,7 +171,7 @@ export default function HomePage() {
         <div className="key">
           <p style={{ color: '#457EFF', fontWeight: 600 }}>
             <Link to={`/addarticle/${selectedPublicationId}/${selectedPostTypeId}`}>
-              <img src={plusImage} alt="Plus" /> Add {selectedPostType}
+              <img src={plusImage} alt="Plus" /> Add {null? "Page":selectedPostType}
             </Link>
           </p>
         </div>
