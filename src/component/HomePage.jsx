640b2d3045a3d2527553e7028af0877112461513
rafi <pawanrajput852710@gmail.com>
2023-10-18 11:05:09 +0530

    done

--- src/component/HomePage.jsx
+++ src/component/HomePage.jsx
@@ -122,7 +122,7 @@ export default function HomePage() {
   // Use the filtering function to get filtered articles based on selectedPublicationId and selectedPostTypeId
   const filteredArticles = filterArticles(articles, selectedPublicationId, selectedPostTypeId, selectedStatusId)
 
-
+  console.log(filteredArticles)
 
   
 
