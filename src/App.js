d2ac7f37f90239bfba6159242b9cd580295cc2e2
rafi <pawanrajput852710@gmail.com>
2023-10-23 13:10:10 +0530

    done

--- src/App.js
+++ src/App.js
@@ -1,30 +1,23 @@
-
 import './App.css';
 import React from 'react';
-import FormPage from './component/FormPage';
-import HomePage from './component/HomePage';
-import { BrowserRouter, Routes, Route } from "react-router-dom"
-import Updatearticle from './component/Updatearticle';
-
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import PrivateRoute from './component/PrivateRoute';
 import Login from './component/Login';
 import SignUp from './component/SignUp';
-
-
+import FormPage from './component/FormPage';
+import Updatearticle from './component/Updatearticle';
+import HomePage from './component/HomePage';
 
 function App() {
-
-
-
   return (
     <div className="App">
       <BrowserRouter>
         <Routes>
-           
-          <Route path ="/login" element ={<Login/>} />
-          <Route path ="/signup" element ={<SignUp/>} />
-           <Route path="/" element={<HomePage/>} /> 
-          <Route path="/addarticle/:publicationId/:postTypeId" element={<FormPage />}/>
-          <Route path='/updatearticle/:articleId' element={<Updatearticle/>}></Route>
+          <Route path="/login" element={<Login />} />
+          <Route path="/signup" element={<SignUp />} />
+          <Route path="/" element={<PrivateRoute Component={HomePage} />} />
+          <Route path="/addarticle/:publicationId/:postTypeId" element={<PrivateRoute Component={FormPage} />} />
+          <Route path="/updatearticle/:articleId" element={<PrivateRoute Component={Updatearticle} />} />
         </Routes>
       </BrowserRouter>
     </div>
