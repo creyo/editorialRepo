6f10ab3825e23f536f5b9f38df748cb69829b543
rafi <pawanrajput852710@gmail.com>
2023-10-23 11:15:14 +0530

    done

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Login from './component/Login';
-import HomePage from './component/HomePage';
-import FormPage from './component/FormPage';
-import Updatearticle from './component/Updatearticle';
+// import HomePage from './component/HomePage';
+// import FormPage from './component/FormPage';
+// import Updatearticle from './component/Updatearticle';
 import SignUp from './component/SignUp';
-import PrivateRoute from './component/PrivateRoute';
+// import PrivateRoute from './component/PrivateRoute';
 
 function App() {
   return (
@@ -14,9 +14,7 @@ function App() {
         <Routes>
           <Route path="/login" element={<Login />} />
           <Route path="/signup" element={<SignUp />} />
-          <PrivateRoute path="/" element={<HomePage />} />
-          <PrivateRoute path="/addarticle/:publicationId/:postTypeId" element={<FormPage />} />
-          <PrivateRoute path="/updatearticle/:articleId" element={<Updatearticle />} />
+          
         </Routes>
       </Router>
     </div>
