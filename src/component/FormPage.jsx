bf7a21d6da070e56697f130111a0be178935c367
rafi <pawanrajput852710@gmail.com>
2023-10-23 18:34:44 +0530

    done

--- src/component/FormPage.jsx
+++ src/component/FormPage.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
-import { findPostTypeNameById, filterItemsByCategoryId } from "./filter"
+import { findPostTypeNameById } from "./filter"
 import ReactQuill from 'react-quill';
 import ProfilePopup from './popUp/ProfilePopup';
 
