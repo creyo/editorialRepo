16e52eb2c228dd40620ba7c17c67b4ae7b453e82
rafi <pawanrajput852710@gmail.com>
2023-10-25 12:33:41 +0530

    done

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ import FormPage from './component/FormPage';
 import Updatearticle from './component/Updatearticle';
 import HomePage from './component/HomePage';
 import NoDataFoundPage from './component/ExtraPage/NoDataFoundPage';
+import Logout from './component/Logout';
 
 function App() {
   return (
@@ -16,6 +17,7 @@ function App() {
         <Routes>
           <Route path="/login" element={<Login />} />
           <Route path="/signup" element={<SignUp />} />
+          <Route path="/logout" element={<Logout />} />
           <Route path="/" element={<PrivateRoute Component={HomePage} />} />
           <Route path="/addarticle/:publicationId/:postTypeId" element={<PrivateRoute Component={FormPage} />} />
           <Route path="/updatearticle/:articleId" element={<PrivateRoute Component={Updatearticle} />} />
---
+++ src/component/Logout.jsx
@@ -0,0 +1,21 @@
+import React, { useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
+import supabase from '../config/supabase';
+
+function Logout() {
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    async function logout() {
+      await supabase.auth.signOut();
+      localStorage.removeItem('sb-narivuecshkbtcueblcl-auth-token');
+      navigate('/login');
+    }
+
+    logout();
+  }, [navigate]);
+
+  return <div>Logging out...</div>;
+}
+
+export default Logout;
